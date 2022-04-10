3642b4a707cf5dde1bff5497eba1891af3ca5548
mithilaaaaaa <102239439+mithilaaaaaa@users.noreply.github.com>
2022-04-10 23:39:45 +0600

    Add files via upload

--- sandbox.js
+++ sandbox.js
@@ -1,137 +1,104 @@
-//function declaration
-function greet() {
-    console.log('helo there')
-}
+// const blogs=[
+//     {title: 'why mac & cheese rules', likes:30},
+//     {title: '10 things to make with marmite',likes:50}
+// ],
 
-greet();
+// console.log(blogs);
 
-// function expressions
-const speak = function() {
-    console.log('good day!')
-};
-speak();
 
-//arguments and parameters
 
-const speak = function(name) {
-    console.log('good day ${name}')
-};
-speak('mario')
+let user={
+    name: 'crysta',
+    age: 30,
+    email: 'crystal@gmail.com',
+    location: 'berlin',
+    //blogs: ['why mac & cheese rules','10 things to make with marmite'],
+    blogs=[
+        {title: 'why mac & cheese rules', likes:30},
+        {title: '10 things to make with marmite',likes:50}
+    ],
 
-// passing different parameter/arguments
-const speak = function(name, time) {
-    console.log('good ${time} ${name}')
-};
-speak('mario', 'morning');
-
-// give this parameters some different values
-const speak = function(name = 'luigi', time = 'night') {
-    console.log('good ${time} ${name}')
-};
-speak();
-
-//returning values
-const calcArea = function(radius) {
-    return 3.14 * radius ** 2;
-};
-const area = calcArea(5);
-console.log(area);
-
-//regular function
-const calcArea = function(radius) {
-    return 3.14 * radius ** 2;
-};
-
-//arrow function
-const calcarea = radius => 3.14 * radius ** 2;
-//two parameters we have to use parenthesis
-
-const area = calcArea(5);
-console.log('area is :', area);
-
-//practise arrow function
-const greet = function() {
-    return 'hello,world';
-};
-const greet = () => 'hello,world';
-const result = greet();
-console.log(result);
-
-// practise arrow function 
-const bill = function(products, taxs) {
-    let total = 0;
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * taxs;
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user loged out');
+    },
+    
+    logBlogs: function(){
+        //console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog.title,blog.likes);
+        })
     }
-    return total;
-}
-const bill = (products, taxs) => {
-    let total = 0;
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * taxs;
-    }
-    return total;
+
 };
 
-console.log(bill([10, 15, 30], 0.2));
+// console.log(user);
+// console.log(user.name);
 
-//method and function 
-const name = 'shaun'
+// //user.age=35
+// console.log(user.age);
 
-//function 
-const greet = () => 'hello';
-let resultOne = greet();
-console.log(resultOne);
+// const key = 'location';
 
+// console.group(user[key]); user['location']
+// user['name']='chun-li';
+// console.log(user['name']);
 
+// console.log(typeof user);
 
-// method
-let resultTwo = name.toUpperCase();
-console.log(resultTwo);
+//--------------------------------------------------------
 
-// callbacks & foreach
-const myFunc = (callbackFunc) => {
-    //do something
-    let value = 50;
-    callbackFunc(value);
-};
-myFunc(value => {
-    //do something
-    console.log(value);
-});
-// foreach
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+// user.login();
+// user.logout();
 
-people.forEach(person => {
-    console.log(person)
-});
-//using index in second argument in arrow function
+// const name = 'mario';
+// name.toUpperCase();
 
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+//--------------------------------------------------------
 
-people.forEach(person, index) => {
-console.log(index, person)
-});
-// callbacks and foreach together 
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
-
-const logPerson = (person, index) => {
-
-    console.log('${index} - hello $ {person}');
-};
-people.forEach(logPerson);
+user.logBlogs();
+console.log(this);
 
 
-// get a reference to the 'ul'
-const ul = document.querySelector('.people');
+//math object
 
-const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+console.log(Math);
+console.log(Math.Pi);
+console.log(Math.E);
 
-let html = ' ';
-people.forEach(person => {
-    // create html template
-    html += '<li style="color: purple">${person}</li>';
-});
+const area= 7.7;
 
-console.log(html);
-ul.innerHTML = html
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.cell(area));
+console.log(Math.trunc(area));
+
+
+//random numbers
+
+const random =Math.random();
+
+console.log(random);
+console.log(Math.round(random*100));
+
+//primitive values
+
+let scoreOne=50;
+let scoreTwo = scoreOne;
+
+console.log('scoreOne:${scoreOne}','scoreTwo: $(scoreTwo}');
+
+scoreOne = 100;
+console.log('scoreOne: ${scoreOne}','scoreTwo: ${scoreTwo}');
+
+//refernce values
+
+const userOne ={ name:'ryu',age:30};
+const userTwo= userOne;
+console.log(userOne, usertwo);
+
+userOne.age =40;
+console.log(userOne, userTwo);
